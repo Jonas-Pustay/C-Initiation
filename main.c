76c56abc911f0fd6ddb710e5a599304d0df230c8
Jonas-Pustay <95695354+Jonas-Pustay@users.noreply.github.com>
2024-02-22 15:39:27 +0000

    modified:   a.out 	modified:   main.c

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 
+static int z = 6;
+
+int str_lenght(char t[])
+{
+    int i = 0;
+
+    for( ; ; ) 
+    { 
+        if (t[i]=='\0') return i;
+        i++;
+    }
+    
+    return i;
+} 
+
 int main()
 {
-    printf("Hello World\n");
+    //var declaration
+    int x;
+
+    x=5;
+
+    int y =0;
+    int a =1, b = 2;
+
+    const float price = 3.14;
+
+    char c= 'a';
+    char s[]="hola";
+
+    for (int i = 0; i < str_lenght(s); i++)
+    {
+        /* code */
+        printf("%c",s[i]);
+    }
+    
+
+    printf("\nx = %d \t y= %i \t price= %.2f \t %c \t %s \t %c \t \n",x,y,price,c,s,s[2]);
     return 0;
 }
